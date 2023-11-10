3edd39a5aa9abe7f322a40c06053f6157ee64c6f
Artur Bernardo <arturberna@gmail.com>
2023-11-10 01:12:47 -0300

    Commit inicial

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+     public float velocidade = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deslocamento = velocidade*Time.deltaTime;
+        transform.Translate(Vector2.right*deslocamento);
+    }
+    void OnTriggerEnter2D(Collider2D body){
+        
+        if(body.gameObject.CompareTag("parede")){
+            transform.position = new Vector3(transform.position.x,transform.position.y-1,transform.position.z);
+            velocidade*=-1.0f;
+            
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocidade = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deslocamento = velocidade*Time.deltaTime;
+        if(Input.GetKey(KeyCode.RightArrow)){
+            transform.Translate(Vector2.right*deslocamento);
+        }else if(Input.GetKey(KeyCode.LeftArrow)){
+            transform.Translate(Vector2.left*deslocamento);
+        }
+        
+    }
+}
